5aa2f85f17fd7eab217dd12d521deead6ada8b7e
Clement <clementbeauseigneur@hotmail.fr>
2015-05-28 18:09:59 +0200

    Scroll in informations added

--- app/src/main/java/axelindustry/sumito4/Informations.java
+++ app/src/main/java/axelindustry/sumito4/Informations.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * Created by Clement on 20/05/2015.
@@ -20,6 +22,7 @@ public class Informations extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infos_layout);
 
-
+        TextView infos = (TextView)this.findViewById(R.id.nomjeu);
+        infos.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
 }
