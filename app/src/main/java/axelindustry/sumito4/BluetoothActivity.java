d5ccb3bebfcd430f9cc271e1418489795b47cd75
astrubel <axel.strubel@telecom-bretagne.eu>
2015-05-31 15:22:27 +0200

    Tests

--- app/src/main/java/axelindustry/sumito4/BluetoothActivity.java
+++ app/src/main/java/axelindustry/sumito4/BluetoothActivity.java
@@ -159,27 +159,5 @@ public class BluetoothActivity extends Activity {
         }
     }
 
-    /**
-     * Created by Bruno Quercia on 15/04/2015.
-     */
-    public static class drawBall {
-        private Bitmap image;
-        private int x, y;
 
-        public void drawBall(Bitmap image, int x, int y){
-            this.image = image;
-            this.x = x;
-            this.y = y;
-        }
-
-        public void move(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-
-        public Bitmap getBitmap()
-        {
-            return image;
-        }
-    }
 }
